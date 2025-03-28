3859c60bc822fe9b404eb06765efe0a8d2eed502
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-03-28 21:19:45 +0600

    feat: adding unit tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -16,17 +16,20 @@ class BaseViewTest(APITestCase):
     def create_task(title="", description="", task_status="", due_date=None):
         if title != "" and description != "" and task_status != "":
 
-            Task.objects.create(
+            task = Task.objects.create(
                 title=title,
                 description=description,
                 status=task_status,
                 due_date=due_date
             )
 
-    def setUp(self):
-        self.create_task(title="Test Task 01", description="Task Description", task_status="initiated")
-        self.create_task(title="Test Task 02", description="Task Description", task_status="in_progress")
+            return task
 
+
+    def setUp(self):
+        task_1 = self.create_task(title="Test Task 01", description="Task Description", task_status="initiated")
+        task_2 = self.create_task(title="Test Task 02", description="Task Description", task_status="in_progress")
+        self.task_id = task_1.id # using for update, retrieve and delete test operation
 
 class GetAllTasksTest(BaseViewTest):
     def test_get_all_tasks(self):
@@ -41,3 +44,43 @@ class GetAllTasksTest(BaseViewTest):
 
         self.assertEqual(response.data, expected_response)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class CreateTaskTest(BaseViewTest):
+    def test_create_task(self):
+        task_data = {
+            "title": "New Task",
+            "description": "Do the coding assignment",
+            "status": "initiated",
+        }
+        response = self.client.post(
+            reverse('tasks-create'),
+            task_data,
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class UpdateTaskTest(BaseViewTest):
+    def test_update_task(self):
+        task_data = {
+            "title": "Updated Task",
+        }
+        response = self.client.patch(
+            reverse('tasks-update', kwargs={'task_id': self.task_id}),
+            task_data,
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class RetrieveTaskTest(BaseViewTest):
+    def test_retrieve_task(self):
+        response = self.client.get(
+            reverse('tasks-retrieve', kwargs={'task_id': self.task_id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class DeleteTaskTest(BaseViewTest):
+    def test_delete_task(self):
+        response = self.client.delete(
+            reverse('tasks-delete', kwargs={'task_id': self.task_id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
